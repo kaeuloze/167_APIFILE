7cae5b069c9d4a3d415f838b8c91d5c1cfd81c6e
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-25 14:25:29 +0700

    Membuat fungsi koneksi database dan proses sinkronisasi Sequelize pada db.js

---
+++ config/db.js
@@ -0,0 +1,19 @@
+db.js
+const mysql = require('../models');
+
+async function connectDatabase(){
+    try{
+        await db.sequelize.authenticate();
+        console.log('Database connected successfully');
+
+        await db.sequelize.sync();
+        console.log('Database syncronized');
+
+    }
+    catch (err) {
+    console.error('Database connecteion failed:', err.message);
+    process.exit(1);
+    }
+}
+
+module.exports = connectDatabase;
